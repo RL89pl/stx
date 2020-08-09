b2fb15632e22d06bf4c92a0db0fd64eb549801e5
Robert Lewandowski <rl89pl@gmail.com>
2020-08-09 22:49:19 +0200

    add models: Book, Category and Author

--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    def __str__(self):
+        return self.name
+        
+class Book(models.Model):
+    title = models.CharField(max_length=155, unique=True, db_index=True)
+    authors = models.ManyToManyField(Author)
+    published_date = models.PositiveIntegerField(blank=True, null=True)
+    categories = models.ManyToManyField(Category)
+    average_rating = models.PositiveIntegerField(blank=True, null=True)
+    ratings_count = models.PositiveIntegerField(blank=True, null=True)
+    thumbnail = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
