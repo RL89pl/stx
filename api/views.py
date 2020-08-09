ca0c511924de23f1e3fad939d11b1c8a5b505d0b
Robert Lewandowski <rl89pl@gmail.com>
2020-08-09 23:08:53 +0200

    change filter attr

--- api/views.py
+++ api/views.py
@@ -10,8 +10,8 @@ class BookView(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     filter_backends = [DjangoFilterBackend,filters.SearchFilter]
-    filterset_fields = ('title','authors',)
-    search_fields = ['title', 'authors']
+    filterset_fields = ('id','title','authors','published_date',)
+    search_fields = ['id','title', 'authors','published_date']
 
 class AuthorView(viewsets.ModelViewSet):
     queryset = Author.objects.all()
